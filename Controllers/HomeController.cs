2838b2aa4951131ac9e8744df9b08be743edd60c
sebastian <sebasvicente10@gmail.com>
2023-08-27 22:27:40 -0500

    Buenas, proyecto futbol

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using practica1.Models;
+
+namespace practica1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult FRegistro(operacion objoperacion)
+    {
+        string rspt;
+
+        try{
+            rspt = objoperacion.msjF().ToString()+"";
+        }catch(Exception ex){
+            rspt = ex.Message;
+        }
+
+        ViewData["Message"] = rspt;
+        return View("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
